43add3bcd21f5d9ef1a2940b440a065cbacae5bd
Alan Coomes <acoomes1@cord.edu>
2020-07-30 02:06:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,10 @@ reverse_sent.join(" ")
 end
 
 def reverse_collect(sentence)
+   reverse_sent = []
+  sent_array = sentence.split
+  sent_array.collect do |word|
+    reverse_sent.push(word.reverse)
+  end
+reverse_sent.join(" ")
+end
